0a352d220ff10daacf0145210cde6f8df6169f05
Spencer Krum <nibz@cat.pdx.edu>
2014-05-29 01:02:20 -0700

    Can now create dummy events and list all events
    This basically means we add events and search across them. This is a
    cool result beacuse it basically means the hard part is over. Unless you
    count graphing in javascript. Bleh.

--- puppetanalytics/app.py
+++ puppetanalytics/app.py
@@ -11,5 +11,35 @@ app = Flask(__name__)
 def hello():
     return "Hello and welcome to Puppet analytics!"
 
+@app.route("/add_dummy")
+def add_dummy():
+    """
+    Add a dummy module download every time this is hit
+    """
+
+    doc = {
+        'author': 'nibz',
+        'name'  : 'puppetboard',
+        'tags' : ['awesome', 'ci', 'production'],
+        'timestamp': datetime.now(),
+    }
+    res = es.index(index="module-downloads", doc_type='modules', body=doc)
+    return str(res['created'])
+
+@app.route("/list_events")
+def list_events():
+    """
+    Do a massive search to find all module install events
+    You probably never want to actually run this
+    """
+    res = es.search(index="module-downloads", body={"query": {"match_all": {}}})
+    response = "<html><body>"
+    response += "<p>Got %d Hits:" % res['hits']['total']
+    for hit in res['hits']['hits']:
+        response += "<p>%(timestamp)s %(author)s: %(name)s %(tags)s" % hit["_source"]
+    response += "</body></html>"
+    return response
+
 if __name__ == "__main__":
+    app.debug = True
     app.run()
