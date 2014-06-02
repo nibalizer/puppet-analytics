5719b375d3b28d9b6c819e1a3e0af7f83a35703d
Gregory Haynes <greg@greghaynes.net>
2014-06-01 22:50:35 -0700

    Removed / page until it is replaced for sqlalchemy

--- puppetanalytics/app.py
+++ puppetanalytics/app.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import Flask, request, url_for, render_template
+from flask import Flask, request, render_template
 
 import db
 from dbapi import (get_all_deployments,
@@ -12,59 +12,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def mainpage():
-    res = es.search(index="module-downloads",
-                    body={
-                        "size": "0",
-                        "aggs": {
-                            "group_by_author": {
-                                "terms": {"field": "author"},
-                                "aggs": {
-                                    "group_by_name": {
-                                        "terms": {"field": "name"}
-                                    }
-                                }
-                            }
-                        }
-                    })
-
-    authors = res['aggregations']['group_by_author']['buckets']
-    response = "<html><body>"
-    response = "<p>Hello and welcome to Puppet Analytics!"
-    response += "<p>Found %d total module downloads" % res['hits']['total']
-    response += "<p>Found %d Authors" % len(authors)
-
-    # I feel really stupid doing it this way, isn't there a good way?
-    modules_by_author = [
-        len(author['group_by_name']['buckets']) for author in
-        res['aggregations']['group_by_author']['buckets']]
-    num_modules = sum(modules_by_author)
-    response += " and %d Modules" % num_modules
-
-    author_module = {}
-
-    for author in res['aggregations']['group_by_author']['buckets']:
-        author_name = author['key']
-        author_module[author_name] = {}
-        for module in author['group_by_name']['buckets']:
-            module_name = module['key']
-            author_module[author_name][module_name] = {
-                'events': module['doc_count']}
-
-    response += "<p>"
-
-    for author, modules in author_module.iteritems():
-        for modulename, data in modules.iteritems():
-            line = "<p>"
-            line += "<a href=\""
-            line += url_for('module_page', author=author, module=modulename)
-            line += "\">"
-            line += author + "/"
-            line += modulename + "</a> "
-            line += str(data['events']) + " deploys"
-            response += line
-
-    response += "</body></html>"
-    return response
+    """
+    The previous page was silly. Please feel free to make a new one.
+    """
+    return "Hi"
 
 
 @app.route("/<author>/<module>")
